26cc40f17deacb33de48efe729e6e27d70edfc2d
Jarex <ucelcemong@gmail.com>
2026-04-26 00:46:06 +0700

    fix database syntax

---
+++ database/database.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+var DB *sql.DB
+
+func Connect() {
+	var err error
+
+	DB, err = sql.Open("sqlite", "expense.db")
+	if err != nil {
+		panic(err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Database connected!")
+
+	createTables()
+}
+
+func createTables() {
+	userTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT
+	);`
+
+	expenseTable := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		user_id INTEGER,
+		title TEXT,
+		amount REAL
+	);`
+
+	_, err := DB.Exec(userTable)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = DB.Exec(expenseTable)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Tables ready!")
+}
